7bef11da40187cedbc138756637b724ea7223604
Leshiro <burak66012@gmail.com>
2026-04-22 18:23:37 +0300

    modification

--- visual/setup/config.py
+++ visual/setup/config.py
@@ -1,14 +1,14 @@
-# settings
-PORT = 'COM4'
-BAUD = 9600
-
-W, H = 1000, 640
-
-MAX_DIST = 50
-MAX_TRAIL = 150
-
+# display settings
 TITLE = "ARDUINO ULTRASONIC RADAR"
 WINDOW_TITLE = f"Arduino Ultrasonic Radar"
+W, H = 1000, 640
+
+# settings
+PORT = 'COM4' # usb port
+BAUD = 9600
+
+MAX_DIST = 50 # distance cap
+MAX_TRAIL = 150 # fadeout delay
 
 # initials
 INITIAL_ANGLE = 90.0
