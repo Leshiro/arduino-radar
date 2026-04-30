09d46e5ea842051256dfc46dcf86bf19c806d3a3
Leshiro <burak66012@gmail.com>
2026-04-30 11:27:13 +0300

    auto port finder

--- visual/setup/config.py
+++ visual/setup/config.py
@@ -1,10 +1,21 @@
+# imports
+import serial.tools.list_ports
+
+# autofind COM port
+def find_arduino_port():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        if any(keyword in (port.description + port.manufacturer or '').lower() for keyword in ['arduino', 'ch340', 'cp210', 'ftdi', 'usb serial']):
+            return port.device
+    return None
+
 # display settings
 TITLE = "ARDUINO ULTRASONIC RADAR"
 WINDOW_TITLE = f"Arduino Ultrasonic Radar"
 W, H = 1000, 640
 
 # settings
-PORT = 'COM4' # usb port
+PORT = find_arduino_port() # usb port
 BAUD = 9600
 
 MAX_DIST = 50 # distance cap
